0c81370ffda2414565f787370d9187f2cbc6bb34
Prakash Arul Mani <prakash.paramanandam@arkinfotec.com>
2021-11-09 10:24:30 +0530

    PhantomJS test

--- src/test/java/SeleniumExample.java
+++ src/test/java/SeleniumExample.java
@@ -12,28 +12,29 @@ public class SeleniumExample {
 
     private static WebDriver driver;
 
-    @BeforeClass
-    public static void openBrowser(){
-
-        if(System.getProperty("webdriver.chrome.driver") != null)
-            driver = new ChromeDriver();
-        else if(System.getProperty("phantomjs_binary_path") != null)
-            driver = new PhantomJSDriver();
-        else
-            throw new RuntimeException("Unknown web driver specified.");
-
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
-
-    @AfterClass
-    public static void closeBrowser(){
-        driver.quit();
-    }
+//     @BeforeClass
+//     public static void openBrowser(){
+//
+//         if(System.getProperty("webdriver.chrome.driver") != null)
+//             driver = new ChromeDriver();
+//         else if(System.getProperty("phantomjs_binary_path") != null)
+//             driver = new PhantomJSDriver();
+//         else
+//             throw new RuntimeException("Unknown web driver specified.");
+//
+//         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//     }
+//
+//     @AfterClass
+//     public static void closeBrowser(){
+//         driver.quit();
+//     }
 
     @Test()
     public void browserInitTest() {
-        driver.get("http://www.google.com/");
-
+        System.setProperty("phantomjs.binary.path", "phantomjs");
+        WebDriver driver = new PhantomJSDriver();
+        driver.get("http://www.google.com");
         Assert.assertEquals(driver.getTitle(),"Google");
     }
 }
